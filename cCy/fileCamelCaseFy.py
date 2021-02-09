dcea5522a73cb952fa96911e29772a5ead061e01
Rafael <rafaeloliveira@tokenlab.com.br>
2021-02-08 22:00:49 -0300

    First code functioning, awating test

---
+++ cCy/fileCamelCaseFy.py
@@ -0,0 +1,16 @@
+from os import path
+import re
+
+def divideFilePath(filepath):
+  head, base = path.split(filepath)
+  ext = ''
+  while '.' in base:
+    base, moreExt = path.splitext(base)
+    ext = moreExt + ext
+  return head, base, ext
+
+def camelCaseFyName(str):
+  noDashStr = re.sub(r'[_\-]', ' ', str)
+  pascalStr = ''.join(x for x in noDashStr.title() if not x.isspace())
+  camelStr = pascalStr[0].lower() + pascalStr[1:]
+  return camelStr
---
+++ cCy/folderWalk.py
@@ -0,0 +1,16 @@
+import fileCamelCaseFy as fCCy
+from os import walk, rename, path
+
+def renameEntity(root, entityPath):
+  head, name, ext = fCCy.divideFilePath(entityPath)
+  newName = fCCy.camelCaseFyName(name)
+  if name != newName:
+    rename(path.join(root, head, name, ext), path.join(root, head, newName, ext))
+
+def folderCamelCaseFy(rootPath):
+  for root, folders, _ in walk(rootPath):
+    for folder in folders:
+      renameEntity(root, folder)
+  for root, _, files in walk(rootPath):
+    for file in files:
+      renameEntity(root, file)
